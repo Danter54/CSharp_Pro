be182b8f4d7aedf4f69ca00045e5272f7480de7f
38066 <38066@DESKTOP-F1NHGC8>
2022-11-27 23:11:12 +0200

    Add new project

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Test:");
+
+int a = 3.4.Round();
+Console.WriteLine(a);
+int b = 3.6.Round();
+Console.WriteLine(b);
+
+public static partial class Extention
+{
+    public static int Round(this double d)
+    {
+        return Convert.ToInt32(Math.Round(d));
+    }
+}
