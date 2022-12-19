b729f84531c5df86cb5c481f0f97f4d0f24d1d52
38066 <38066@DESKTOP-F1NHGC8>
2022-12-19 20:53:24 +0200

    add hw4

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,18 @@
+﻿var list =new List<int> {1,2,3,4,5,6};
+var newlist = list.MyWhere(x=> x % 3 == 0).ToList();
+
+foreach (var item in newlist)
+{
+    Console.Write($"{item} ");
+}
+
+static class Extention {
+    public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> filter)
+    {
+        foreach (var item in source)
+        {
+            if (filter(item))
+                yield return item;
+        }
+    }
+}
